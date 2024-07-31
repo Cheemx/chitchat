23e88c562eb33b1a34fd6568324625059bf6e4ef
Cheemx <chinmaymahajan999@gmail.com>
2024-07-31 09:10:59 +0530

    changed env reading and env location

--- server/main.go
+++ server/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"api/controller"
 	"api/db"
 	"api/router"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func init() {
-	controller.LoadEnvVariables()
+	// controller.LoadEnvVariables()
+	er := godotenv.Load(".env")
+
+	if er != nil {
+		log.Fatalf("Error loading .env file %v", er)
+	}
 	db.ConnectToDB()
 }
 
